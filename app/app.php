427181a7792a6ebb0dd315e3e06ef6839ecc7424
Jason Wuerch <wuerchjason@gmail.com>
2016-03-04 08:26:17 -0800

    create initial files and directories

--- app/app.php
+++ app/app.php
@@ -7,15 +7,7 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
     $app->get('/', function() use ($app) {
-      return $app['twig']->render('home.html.twig');
-    });
-
-    $app->post('/answer', function() use ($app) {
-      $matcher = new WordMatch;
-      $input1 = $_POST['word'];
-      $input2 = $_POST['phrase'];
-      $result = $matcher->checkMatch($input1, $input2);
-      return $app['twig']->render('home.html.twig', array('result' => $result, 'word' => $input1, 'phrase' => $input2));
+      return $app['twig']->render('index.html.twig');
     });
 
     return $app;
--- src/WordMatch.php
+++ src/WordMatch.php
@@ -1,20 +1,3 @@
 <?php
-    class WordMatch {
-      function checkMatch($word, $phrase) {
-        foreach (str_split($word) as $char) {
-          if (preg_match('/[!#;@._?,]/', $char)) {
-            return 'Please enter a word without punctuation.';
-          }
-        }
-        $phrase = strtolower(preg_replace('/[!#;@,._?]+/u', '', $phrase));
-        $phrase = explode(' ', $phrase);
-        $counter = 0;
-        foreach ($phrase as $each_word) {
-          if ($each_word == $word) {
-            $counter++;
-          }
-        }
-        return $counter;
-      }
-    }
+
 ?>
