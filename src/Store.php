fa34d0476a60056c45b3239e75a5710cb0997d8a
Jason Wuerch <wuerchjason@gmail.com>
2016-03-04 09:00:22 -0800

    create construct for store class

--- src/Store.php
+++ src/Store.php
@@ -1,3 +1,15 @@
 <?php
 
+    class Store {
+        private $name;
+        private $location;
+        private $id;
+
+        public function __construct($name, $location, $id) {
+            $this->name = $name;
+            $this->location = $location;
+            $this->id = $id;
+        }
+    }
+
 ?>
