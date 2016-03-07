47b613257b11a9a60c692d455b51af261bb23cb2
Jason Wuerch <Jason Wuerch>
2016-03-07 11:04:32 -0800

    add city class getters/settings in php class

---
+++ src/ZipCode.php
@@ -0,0 +1,31 @@
+<?php
+
+    class ZipCode {
+        private $number;
+        private $city_id;
+        private $id;
+
+        public function __construct($number, $city_id, $id = null) {
+            $this->number = $number;
+            $this->city_id = $city_id;
+            $this->id = $id;
+        }
+
+        //Setters
+        public function setNumber($new_number) {
+            $this->number = $new_number;
+        }
+        //Getters
+        public function getNumber() {
+            return $this->$number;
+        }
+        public function getCityId() {
+            return $this->city_id;
+        }
+        public function getId() {
+            return $this->id;
+        }
+    }
+
+
+ ?>
