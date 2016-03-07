782a6f0504dc0fd7925760a00376b8cbf6fcd0bb
Jason Wuerch <Jason Wuerch>
2016-03-07 11:05:50 -0800

    add zip code class getters/setters in php class. The last commit was a mistake and meant to say zip code over city

---
+++ tests/ZipCodeTest.php
@@ -0,0 +1,20 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+    $server = 'mysql:host=localhost;dbname=poly_date_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    require_once "src/ZipCode.php";
+
+    class ZipCodeTest extends PHPUnit_Framework_TestCase {
+
+        protected function tearDown() {
+
+        }
+    }
+
+?>
