19fbda388984b19c8d4ee1837d9cb15ecddbb05c
Jason Wuerch <Jason Wuerch>
2016-03-07 10:29:05 -0800

    pass spec for getting age of user

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -111,6 +111,30 @@
             //Assert;
             $this->assertEquals($kink_friendly, $result);
         }
+
+        function testAge() {
+            //Arrange;
+            $identity = 'Male';
+            $name = 'Jason';
+            $status = 'Single';
+            $kink_friendly = true;
+            $age = 27;
+            $display_name = 'JMoney';
+            $email = 'wuerchjason@gmail.com';
+            $about_me = 'I am friendly.';
+            $gender = 'Male';
+            $interests = 'Basketball, Tennis';
+            $seeking_gender = 'Female';
+            $seeking_relationship_type = 'Primary Partner';
+            $last_login = '1989-03-07';
+            $test_user = new User($identity, $name, $status, $kink_friendly, $age, $display_name, $email, $about_me, $gender, $interests, $seeking_gender, $seeking_relationship_type, $last_login);
+
+            //Act;
+            $result = $test_user->getAge();
+
+            //Assert;
+            $this->assertEquals($age, $result);
+        }
     }
 
 
