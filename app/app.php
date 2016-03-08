883660e82468c9625d527c460ec9fac98495272f
Jason Wuerch <Jason Wuerch>
2016-03-07 16:30:52 -0800

    create basic templates for index, header and footer pages

--- app/app.php
+++ app/app.php
@@ -1,6 +1,14 @@
 <?php
     require_once __DIR__.'/../vendor/autoload.php';
-    require_once __DIR__.'/../src/WordMatch.php';
+    require_once __DIR__.'/../src/User.php';
+
+    $app = new Silex\Application();
+    $server = 'mysql:host=localhost;dbname=poly_date';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
 
     $app = new Silex\Application();
 
@@ -9,6 +17,22 @@
     $app->get('/', function() use ($app) {
       return $app['twig']->render('index.html.twig');
     });
+
+    $app->get('/register', function() use ($app) {
+        return $app['twig']->render('register.html.twig');
+    });
+
+    $app->get('/user_profile/{id}', function($id) use ($app) {
+
+        return $app['twig']->render('user_profile.index.twig');
+    });
+
+    $app->post('/register_new_user', function() use ($app) {
+
+        return $app['twig']->render('new_profile_created.index.twig');
+    });
+
+
 
     return $app;
 
