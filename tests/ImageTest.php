4e6dd8d62a172ad7f6b90b60faca89b4488fa668
Jason Wuerch <wuerchjason@gmail.com>
2016-03-09 14:39:15 -0800

    pass spec for getting user id for image

--- tests/ImageTest.php
+++ tests/ImageTest.php
@@ -44,6 +44,34 @@
             $this->assertEquals($description, $result);
         }
 
+        function testgetImage() {
+            //Arrange;
+            $username = 'jwuerch';
+            $password = 'xyz';
+            $first_name = 'Jason';
+            $status = 'Single';
+            $kink_friendly = 1;
+            $birthday = '1989-03-07';
+            $last_name = 'JMoney';
+            $email = 'wuerchjason@gmail.com';
+            $about_me = 'I am friendly.';
+            $interests = 'Basketball, Tennis';
+            $seeking_relationship_type = 'Primary Partner';
+            $last_login = '1989-03-07';
+            $test_user = new User($username, $password, $first_name, $last_name, $status, $kink_friendly, $birthday, $email, $about_me, $interests, $seeking_relationship_type, $last_login);
+
+            $title = 'Me';
+            $description = '';
+            $user_id = $test_user->getId();
+            $test_image = new Image($title, $description, $user_id);
+
+            //Act;
+            $result = $test_image->getUserId();
+
+            //Assert;
+            $this->assertEquals($user_id, $result);
+        }
+
 
     }
 
