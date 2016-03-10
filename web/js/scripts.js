2bf3e384159aa6d0a6371be626df9f40af85dac5
Jason Wuerch <wuerchjason@gmail.com>
2016-03-09 18:47:51 -0800

    change users page on app to reflect specific image specs

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -4,8 +4,8 @@
 $(document).ready(function() {
     $("#log-in").click(function(event) {
         $('.sign-in-box').css('opacity', 0).slideDown(1000).animate({ opacity: 1 },{ queue: false, duration: 2000 });
-        event.preventDefault();
         $(".transparency").show();
+        event.preventDefault();
 
     });
  });
